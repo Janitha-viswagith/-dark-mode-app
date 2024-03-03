dba4f7f70df69c2d0ce66c581f0bdb4d2d78d358
Janitha-viswagith <vishwagithjanith@gmail.com>
2024-03-03 08:57:29 +0530

    add App

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+   
+    <div className="App">
+      <h1>Hello World</h1>
+     <div className="box">
+      <h2>This is a box</h2>
+      </div> 
+    </div>
+   ) 
+}
+
+export default App
