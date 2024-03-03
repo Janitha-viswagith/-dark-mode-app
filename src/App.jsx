9cc0fe522a318e74ae1ea30ae9838992a182204b
Janitha-viswagith <vishwagithjanith@gmail.com>
2024-03-03 09:21:30 +0530

    add Toggle,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+   const [isDark, setIsDark] = useState(false)
 
   return (
    
-    <div className="App">
+    <div className="App" data-theme={isDark ? "dark" : "light"} >
       <h1>Hello World</h1>
      <div className="box">
       <h2>This is a box</h2>
